d09d62d465ec9c4fe0cbda421f9d2840afba43ec
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-02-24 23:04:52 +0530

    feat: able to switch between components through nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hideHome, setHideHome] = useState(true);
+  const [hideProducts, setHideProducts] = useState(false);
+  const [hideCart, setHideCart] = useState(false);
+
+  function home() {
+    setHideHome(true);
+    setHideProducts(false);
+    setHideCart(false);
+  }
+
+  function products() {
+    setHideHome(false);
+    setHideProducts(true);
+    setHideCart(false);
+  }
+
+  function cart() {
+    setHideHome(false);
+    setHideProducts(false);
+    setHideCart(true);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <ul className="navBar">
+        <li></li>
+        <li onClick={home}>Home</li>
+        <li onClick={products}>Products</li>
+        <li onClick={cart}>Cart</li>
+      </ul>
+      <div>{hideHome && <Home />}</div>
+      <div>{hideProducts && <Products />}</div>
+      <div>{hideCart && <Cart />}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>We Are Home</div>
+  )
+}
+
+export default Home
